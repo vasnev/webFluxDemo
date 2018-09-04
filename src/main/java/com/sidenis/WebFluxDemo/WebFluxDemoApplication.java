4d7d7f01ae508193bfa6cce5e30963af9a630704
Fedor Vasnev <vasnev@bk.ru>
2018-09-04 22:25:02 +0300

    init

---
+++ src/main/java/com/sidenis/WebFluxDemo/WebFluxDemoApplication.java
@@ -0,0 +1,12 @@
+package com.sidenis.WebFluxDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxDemoApplication.class, args);
+	}
+}
